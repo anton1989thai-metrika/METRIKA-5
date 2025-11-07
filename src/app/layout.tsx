b3fbe9223607ed6075c570822dcd283ec5fbcd0c
anton1989thai-metrika <anton1989thai@gmail.com>
2025-11-07 19:18:31 +0700

    Load Builder editor tools in design mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
+import BuilderEditorBridge from "@/components/BuilderEditorBridge";
 import { Toaster } from "sonner";
 
 const geistSans = Geist({
@@ -33,6 +34,7 @@ export default function RootLayout({
           {children}
           <Toaster />
         </Providers>
+        <BuilderEditorBridge />
       </body>
     </html>
   );
